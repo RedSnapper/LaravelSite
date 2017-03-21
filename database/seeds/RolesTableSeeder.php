35cac39de5268be7e454ae1f451ee95bf4b781f5
= <ben@redsnapper.net>
2017-03-21 15:55:57 +0000

    Moved formlets around..

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		factory(\App\Role::class, 5)->create();
+	}
+
+}
