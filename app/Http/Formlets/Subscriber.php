f7b47009c92d08ec10b0410030dcdebefc649856
= <ben@redsnapper.net>
2017-03-22 11:07:12 +0000

    Generic Subscriber Class

---
+++ app/Http/Formlets/Subscriber.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Part of form
+ * User: ben ©2017 Red Snapper Ltd.
+ * Date: 22/03/2017 10:53
+ */
+
+namespace App\Http\Formlets;
+
+use App\Role;
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Fields\Checkbox;
+
+class Subscriber extends Formlet {
+	/**
+	 * @var Role
+	 * Any hasMany table that wants to use this must have a reciprocal mm() method.
+	 */
+	protected $master;
+
+	public function prepareForm(){
+		$this->master  = $this->model;
+		$items = $this->master->mm()->getRelated()->all();
+		foreach ($items as $item) {
+			$this->add((new Checkbox('',$item->id))
+				->setLabel($item->name)
+			);
+		}
+		$this->model  = $this->master->mm()->get();
+	}
+
+	public function edit() : Model {
+		$this->master->mm()->sync($this->fields());
+		return $this->master;
+	}
+
+	public function persist(): Model {
+		//the model (with key) must be set by the new $master.
+		$this->model->mm()->sync($this->fields());
+		return $this->model;
+	}
+
+
+}
