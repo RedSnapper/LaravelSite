4129fdabdc20e4e80ee98f0fc77007fb3fa50656
Param Dhaliwal <paramdhal@gmail.com>
2017-04-05 15:02:48 +0100

    Migration for category_role_table

---
+++ database/migrations/2017_04_05_140110_create_category_role_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::create('category_role', function (Blueprint $table) {
+			$table->integer('category_id')->unsigned();
+			$table->integer('role_id')->unsigned();
+
+			$table->foreign('category_id')
+			  ->references('id')
+			  ->on('activities')
+			  ->onDelete('cascade');
+
+			$table->foreign('role_id')
+			  ->references('id')
+			  ->on('roles')
+			  ->onDelete('cascade');
+
+			$table->primary(['role_id', 'category_id']);
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		Schema::table('category_role', function (Blueprint $table) {
+			$table->dropForeign('category_role_category_id_foreign');
+			$table->dropForeign('category_role_role_id_foreign');
+		});
+
+		Schema::dropIfExists('category_role');
+    }
+}
