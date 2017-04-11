3b6a663a689041ddb341812eaf247c347cdb3539
Param Dhaliwal <paramdhal@gmail.com>
2017-04-11 10:36:31 +0100

    Moved scout engine into app

---
+++ app/Providers/ElasticsearchProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use App\ScoutEngines\ElasticsearchEngine;
+use Laravel\Scout\EngineManager;
+use Illuminate\Support\ServiceProvider;
+use Elasticsearch\ClientBuilder as ElasticBuilder;
+
+class ElasticsearchProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        resolve(EngineManager::class)->extend('elasticsearch', function($app) {
+            return new ElasticsearchEngine(ElasticBuilder::create()
+                ->setHosts(config('scout.elasticsearch.hosts'))
+                ->build(),
+                config('scout.elasticsearch.index')
+            );
+        });
+    }
+}
