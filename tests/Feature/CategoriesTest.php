cc666a30941190a015f9be035f76312de9005201
Param Dhaliwal <paramdhal@gmail.com>
2017-04-21 10:42:39 +0100

    Setup test datbase

--- tests/Feature/CategoriesTest.php
+++ tests/Feature/CategoriesTest.php
@@ -2,19 +2,49 @@
 
 namespace Tests\Feature;
 
+use App\Models\Activity;
+use App\Models\Category;
+use App\Models\Role;
+use App\Models\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CategoriesTest extends TestCase {
-	use DatabaseMigrations;
+	use DatabaseTransactions;
 
 	/** @test */
 	function guests_may_not_create_categories() {
-		//$this->withExceptionHandling();
+
 		$this->post('/api/category')
 		  ->assertRedirect('/login');
+	}
+
+	/** @test */
+	function an_authenticated_user_can_create_a_category() {
+
+		$root = create(Category::class);
+
+		$user = create(User::class);
+
+		$role = create(Role::class);
+		$role->givePermissionToCategory($root);
+
+		$user->roles()->save($role);
+
+		$this->signIn($user);
+
+		$child = make(Category::class,['parent'=>$root->id]);
+
+		$response = $this->json('POST', '/api/category', $child->toArray());
+
+		$response
+		  ->assertStatus(201);
+
+		//$this->get($response->headers->get('Location'))
+		//  ->assertSee($child->name)
+		//  ->assertSee($thread->body);
 	}
 
 }
