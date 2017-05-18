0639c7f54a78f7b151e88f3626582def80dfd620
Ben Griffin <ben@redsnapper.net>
2017-05-18 14:48:59 +0100

    started on tests

--- tests/Feature/CategoriesTest.php
+++ tests/Feature/CategoriesTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Team;
 use App\Models\User;
 use Tests\TestCase;
 
@@ -24,18 +25,28 @@ class CategoriesTest extends TestCase {
 			->assertStatus(422);
 	}
 
-	/** @test */
+	/** @test All these access tests need putting into their own functions. */
 	function an_authenticated_user_can_create_a_category() {
 
 		$param = User::find(1);
 		$user = $this->signIn($param);
-		assertTrue($user->can('MEDIA_ACCESS'));
+		//not without a team.
+		$this->assertTrue($user->cannot('MEDIA_ACCESS'));
 
 		$category = Category::section("MEDIA")->first()->descendants(false)->first();
-		//Source
 
-		//Needs a team also
-		assertTrue($user->cannot('access',[$category]));
+		//the user can access the category, but not .
+		$this->assertTrue($user->can('ACCESS',[$category]));
+		//in the scope of media..
+		$this->assertTrue($user->cannot('MEDIA_ACCESS',[$category]));
+
+		$team = Team::first(); //This is Otsuka, by default Param has media access to this.
+
+		//These are 'magic' activities.
+		$this->assertTrue($user->can('MEDIA_ACCESS',[$team]));
+		$this->assertTrue($user->can('MEDIA_ACCESS',[$team,$category]));
+		$this->assertTrue($user->can('MEDIA_MODIFY',[$team]));
+		$this->assertTrue($user->can('MEDIA_MODIFY',[$team,$category]));
 
 	}
 
